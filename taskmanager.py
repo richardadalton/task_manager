52a754187efe8056ef76fd7a914574c485cf18d1
Richard Dalton <richard@codeinstitute.net>
2018-10-26 14:02:37 +0000

    Can save task changes, and move between categories

--- taskmanager.py
+++ taskmanager.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import os
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -26,7 +26,7 @@ def get_tasks():
 
 
 @app.route("/tasks/<category>")
-def get_tasks_by_collection(category):
+def get_tasks_by_category(category):
     tasks = mongo.db[category].find()
     return render_template("tasks.html", tasks=tasks)
 
@@ -45,13 +45,23 @@ def add_task():
         return render_template("addtask.html", categories=categories)
 
 
-@app.route('/tasks/<category>/<task_id>/edit')
+@app.route('/tasks/<category>/<task_id>/edit', methods=["GET", "POST"])
 def edit_task(category, task_id):
-    the_task =  mongo.db[category].find_one({"_id": ObjectId(task_id)})
-
-    categories = get_category_names()
-    return render_template('edittask.html', task=the_task, categories=categories)
-
+    if request.method=="POST":
+        form_values = request.form.to_dict()
+        form_values["is_urgent"] = "is_urgent" in form_values
+        tt = mongo.db[category].update({"_id": ObjectId(task_id)}, form_values)
+        
+        if form_values["category_name"] != category:
+            the_task = mongo.db[category].find_one({"_id": ObjectId(task_id)})
+            mongo.db[category].remove(the_task)
+            mongo.db[form_values["category_name"]].insert(the_task)
+            
+        return redirect(url_for("get_tasks_by_category", category=category))
+    else:
+        the_task =  mongo.db[category].find_one({"_id": ObjectId(task_id)})
+        categories = get_category_names()
+        return render_template('edittask.html', task=the_task, categories=categories)
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
